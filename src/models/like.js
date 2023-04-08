f4deafe88c42ee68f4d45ff35caed96d0c3e31f2
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2023-04-08 11:08:52 +0530

    Added user schema

--- src/models/like.js
+++ src/models/like.js
@@ -10,6 +10,11 @@ const likeSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required:true,
         refPath: 'onModel'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 }, { timestamps: true })
 
