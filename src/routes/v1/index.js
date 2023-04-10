18c4a617fd24051fd47d1ed073316eb2ae718cb8
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2023-04-10 15:47:32 +0530

    toggle like router added

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const createTweet = require("../../controllers/tweet-controller")
+const createTweet = require("../../controllers/tweet-controller");
+const toggleLike = require("../../controllers/like-controller");
 
 const router = express.Router();
 
-router.post('/tweet', createTweet)
+router.post('/tweet', createTweet);
+
+router.post('/likes/toggle', toggleLike);
 
 module.exports = router
